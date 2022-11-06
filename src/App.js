09978595c938f4951aa8944ffa38e5628cf97cab
garry m cacho <gc120978levelup1@gmail.com>
2022-11-06 10:49:07 +0800

    TW + MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        Hello world! <MenuIcon/>
     </h1>
     </div>
   );
